7602c99c1ef70d9eabe761f128ebafad00e3b879
Loyio <i@loyio.me>
2021-01-04 22:45:12 +0800

    complete the project 4

--- Chapter4/fill/Fill.asm
+++ Chapter4/fill/Fill.asm
@@ -12,3 +12,51 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+    @status
+    M=-1    // set status black 0xFFFF
+    D=0     // set argument for setscreen
+    @SETSCREEN
+    0;JMP
+
+(LOOP)
+    @KBD
+    D=M     // D = pressing keyboard button character
+    @SETSCREEN
+    D;JEQ
+    D=-1
+
+(SETSCREEN)
+    @ARG    // get the argument D
+    M=D
+    @status
+    D=D-M   // D = D - status
+    @LOOP
+    D;JEQ   // if new status == old status
+
+    @ARG
+    D=M
+    @status
+    M=D     // status = ARG
+
+    @SCREEN
+    D=A     // D = SCREEN address
+    @8192
+    D=D+A   // D = the last SCREEN pixel address
+    @i
+    M=D     // i = D
+
+(SETLOOP)
+    @i
+    D=M-1   // i = i - 1
+    M=D
+    @LOOP
+    D;JLT   // if i < 0 then jump to LOOP
+
+    @status
+    D=M     // D = status
+    @i
+    A=M
+    M=D     // set address i value to status
+    @SETLOOP
+    0;JMP
--- Chapter4/mult/mult.asm
+++ Chapter4/mult/mult.asm
@@ -7,3 +7,27 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+    @R2
+    M=0     // set R2 = 0
+    @i
+    M=0     // set i = 0
+(LOOP)
+    @i
+    D=M     // set D = i
+    @R0
+    D=D-M   // D = i - R0
+    @LOOP_END
+    D;JEQ   // if D >= 0 jump to end
+    
+    @R1
+    D=M     // set D = R1
+    @R2
+    M=D+M   // set M = D + R2
+    @i
+    M=M+1   // i=i+1
+    @LOOP
+    0;JMP
+(LOOP_END)
+    @LOOP_END
+    0;JMP
