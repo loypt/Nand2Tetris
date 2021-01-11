556d830b6033da9682516cf2020d8768814eec80
Loyio <i@loyio.me>
2021-01-11 14:39:20 +0800

    add project 11 code

---
+++ Project11/JackAnalyzer/JackAnalyzer.py
@@ -0,0 +1,31 @@
+#!/opt/homebrew/bin/python3
+"""
+ @Project: JackAnalyzer
+ @Author: Loyio
+ @Date: 2021/1/11
+"""
+import sys, os, os.path, glob
+from Parser import *
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: JackAnalyzer [file.jack|dir]")
+    else:
+        infiles = get_files(sys.argv[1])
+    analyze(infiles)
+
+
+def get_files(file_or_dir):
+    if file_or_dir.endswith('.jack'):
+        return [file_or_dir]
+    else:
+        return glob.glob(file_or_dir + '/*.jack')
+
+
+def analyze(infiles):
+    for infile in infiles:
+        Parser(infile)
+
+
+main()
